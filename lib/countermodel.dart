94bce563d03d3f4ff8aeefb067449e9ce2ee929e
christian@voegtle.org <christian@voegtle.org>
2019-09-14 20:58:11 +0200

    Key/Value Store verwenden, um Daten zu speichern, die einen Neustart der Anwendung überleben sollen

--- lib/countermodel.dart
+++ lib/countermodel.dart
@@ -1,20 +1,43 @@
-
 import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CounterModel extends ChangeNotifier {
+  final String STORED_COUNTER = 'counter';
   int counter = 0;
 
-  CounterModel({int counter}) {
-    this.counter = counter != null ? counter : 0;
+  SharedPreferences preferences;
+
+  CounterModel() {
+    _setupDataStore();
   }
 
   incrementCounter() {
-    this.counter++;
+    _updateCounter(this.counter + 1);
+  }
+
+  decrementCounter() {
+    _updateCounter(this.counter - 1);
+  }
+
+  void _setupDataStore() {
+    SharedPreferences.getInstance().then((prefs) => _init(prefs));
+  }
+
+  _updateCounter(int newCounter) {
+    this.counter = newCounter;
+    _storeCounter();
     notifyListeners();
   }
-  
-  decrementCounter() {
-    this.counter--;
-    notifyListeners();
+
+  _init(SharedPreferences preferences) {
+    this.preferences = preferences;
+    var storedCounter = preferences.getInt(STORED_COUNTER);
+    if (storedCounter != null) {
+      _updateCounter(storedCounter);
+    }
+  }
+
+  void _storeCounter() {
+    this.preferences.setInt(STORED_COUNTER, this.counter);
   }
 }
